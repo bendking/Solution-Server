8689a69db23346357e845603c6c09dc03d479207
Ofec Israel <ofec2000@gmail.com>
2019-01-14 10:25:15 +0200

    bug fix

--- Solver/SearcherSolver.h
+++ Solver/SearcherSolver.h
@@ -14,6 +14,7 @@ class SearcherSolver : public Solver<Searchable<T>*, State<T>*> {
 private:
     Searcher<T>* searcher;
 public:
+    SearcherSolver() = default;
     explicit SearcherSolver(Searcher<T>* _searcher);
     ~SearcherSolver() override;
     State<T>* solve(Searchable<T>* searchable) override;
@@ -24,5 +25,22 @@ public:
  * SearcherSolver<Cell>* solver = new SearcherSolver<Cell>();
  * Solver<Searchable<Cell>, State<Cell>*>* searcherSolver = new SearcherSolver<Cell>(BreadthFS);
  */
+
+
+
+template<class T>
+SearcherSolver<T>::SearcherSolver(Searcher<T>* _searcher) {
+    searcher = _searcher;
+}
+
+template<class T>
+SearcherSolver<T>::~SearcherSolver() {
+    delete searcher;
+}
+
+template<class T>
+State<T>* SearcherSolver<T>::solve(Searchable<T>* searchable) {
+    return searcher->search(searchable);
+}
 
 #endif //SOLUTION_SERVER_SEARCHERSOLVER_H
